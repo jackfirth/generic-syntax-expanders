4b1882eb5e116ad37bb9483af42af9f8f7d2b815
JackFirth <jackhfirth@gmail.com>
2014-12-10 22:11:17 -0800

    Fix deps
    Add scribble dep

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 
 (define name "generic-syntax-expanders")
-(define deps '("base" "predicates"))
+(define deps '("base" "predicates" "scribble-lib"))
 (define scribblings '(("scribblings.scrbl")))

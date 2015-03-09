9d7e1ef09f5781d9ac3e47380cabd3e49b209e93
Jack Firth <jackhfirth@gmail.com>
2015-03-09 09:55:21 -0700

    Add reps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base" "rackunit-lib"))
+(define deps '("base" "rackunit-lib" "fancy-app" "mischief"))
 (define build-deps '("cover"
                      "scribble-lib"
                      "rackunit-lib"

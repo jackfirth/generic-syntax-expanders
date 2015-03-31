fd863a3f88e8b347bc9eac5051db10af215512a0
Jack Firth <jackhfirth@gmail.com>
2015-03-31 16:59:53 -0700

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,9 +1,23 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base" "rackunit-lib" "fancy-app" "mischief" "lenses"
-               "point-free" "predicates" "scribble-lib" "scribble-text-lib"))
-(define build-deps '("cover"
-                     "scribble-lib"
-                     "rackunit-lib"
-                     "racket-doc"))
+
+
+(define deps
+  '("base"
+    "rackunit-lib"
+    "fancy-app"
+    "mischief"
+    "lenses"
+    "point-free"
+    "predicates"
+    "scribble-lib"
+    "scribble-text-lib"))
+
+
+(define build-deps
+  '("cover"
+    "scribble-lib"
+    "rackunit-lib"
+    "racket-doc"
+    "package-scribblings-tools"))

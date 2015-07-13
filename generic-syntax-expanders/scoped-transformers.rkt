bddd41761d249b8f6e2cf48ceb59aa4c8b803b4f
Jack Firth <jackhfirth@gmail.com>
2015-07-13 12:06:10 -0700

    Fix lens-transform argument order

--- generic-syntax-expanders/scoped-transformers.rkt
+++ generic-syntax-expanders/scoped-transformers.rkt
@@ -7,7 +7,7 @@
          with-scoped-pre-transformers)
 
 (define ((with-scoped-pre-transformer transformer stx-lens pre-transformer) stx)
-  (transformer (lens-transform stx-lens pre-transformer stx)))
+  (transformer (lens-transform stx-lens stx pre-transformer)))
 
 (define (with-scoped-pre-transformers transformer pre-transformer-lens-pairs)
   (match pre-transformer-lens-pairs
